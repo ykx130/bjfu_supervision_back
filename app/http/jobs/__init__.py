53ffa69a11bb02a5387027da1fef9025f314df2b
suchang <suchang@zhihu.com>
2018-07-07 14:58:51 +0800

    init project

---
+++ app/http/jobs/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/7 2:53 PM
+# @Author  : suchang
+# @File    : __init__.py.py
---
+++ app/utils/log/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/7 2:54 PM
+# @Author  : suchang
+# @File    : __init__.py.py
