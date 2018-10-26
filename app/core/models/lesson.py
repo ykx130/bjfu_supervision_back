b77f82df8e3e1aefeb36ce2197efe346e6c2aca9
suchang <1103232282@qq.com>
2018-10-26 22:34:38 +0800

    消息

--- app/core/models/lesson.py
+++ app/core/models/lesson.py
@@ -90,7 +90,6 @@ class NoticeLesson(db.Model):
     lesson_id = db.Column(db.String(32), default=-1)
     assign_group = db.Column(db.String(32), default="")
     term = db.Column(db.String(32), default="")
-    status = db.Column(db.String(32), default="")
     votes = db.Column(db.Integer, default=0)
     notice_reason = db.Column(db.String(128), default="")
     notices = db.Column(db.Integer, default=0)
