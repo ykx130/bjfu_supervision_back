cd81b53846f156b02f032c88ff5caff91ac27c20
suchang <1103232282@qq.com>
2018-10-31 12:10:22 +0800

    问卷数据增加对多选的处理

--- app/core/services/form_service.py
+++ app/core/services/form_service.py
@@ -121,23 +121,37 @@ def calculate_map(meta_name):
 
     for form in forms:
         for v in form.get("values"):
-            if v.get('item_type') not in ["radio_option"]:
-                # 分布只计算单选和多选
-                continue
-            if not item_map.get(v['item_name']):
-                # 初始化
-                point = {o['value']: {"option": o, "num": 0} for o in v.get("payload", {}).get("options", [])}
-                point[v['value']]['num'] = point[v['value']]['num'] + 1
-                item_map[v['item_name']] = {
-                    "item_name": v['item_name'],
-                    "point": list(point.values())
-                }
-            else:
-                # 存在直接+1
-                point = item_map[v['item_name']]["point"]
-                for p in point:
-                    if p['option']['value'] == v['value']:
-                        p['num'] = p['num'] + 1
+            if v.get('item_type') == "radio_option":
+                # 处理单选
+                if not item_map.get(v['item_name']):
+                    # 初始化
+                    point = {o['value']: {"option": o, "num": 0} for o in v.get("payload", {}).get("options", [])}
+                    point[v['value']]['num'] = point[v['value']]['num'] + 1
+                    item_map[v['item_name']] = {
+                        "item_name": v['item_name'],
+                        "point": list(point.values())
+                    }
+                else:
+                    # 存在直接+1
+                    point = item_map[v['item_name']]["point"]
+                    for p in point:
+                        if p['option']['value'] == v['value']:
+                            p['num'] = p['num'] + 1
+            if v.get("item_type") == "checkbox_option":
+                # 处理多选
+                if not item_map.get(v['item_name']):
+                    point = {o['value']: {"option": o, "num": 0} for o in v.get("payload", {}).get("options", [])}
+                    for value in v["value"]:
+                        point[value]["num"] = point[value]["num"] + 1
+                    item_map[v['item_name']] = {
+                        "item_name": v['item_name'],
+                        "point": list(point.values())
+                    }
+                else:
+                    point = item_map[v['item_name']]["point"]
+                    for p in point:
+                        if p['option']['value'] in v['value']:
+                            p['num'] = p['num'] + 1
     redis_cli.set("form_service:{}:map".format(meta_name), json.dumps({'item_map': list(item_map.values())}))
 
 
