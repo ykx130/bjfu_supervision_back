35c9af786eafac44c5d0564699a5f712caca176c
zhandong <1468767640@qq.com>
2019-04-29 15:42:34 +0800

    change some bug

--- app/utils/url_condition/url_args_to_dict.py
+++ app/utils/url_condition/url_args_to_dict.py
@@ -1,7 +1,13 @@
+import json
+
 def args_to_dict(condition):
     condition_fin = dict()
     for key in condition:
         for value in condition.getlist(key):
+            try:
+                value = json.loads(value)
+            except:
+                value = value
             if key not in condition_fin:
                 condition_fin[key] = [value]
             else:
