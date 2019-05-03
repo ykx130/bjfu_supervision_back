18f3f83a27d9963ad3db9d8a7f178ccf0e21c4cf
suchang <1103232282@qq.com>
2019-05-04 00:19:36 +0800

    修改首页数据计算

--- app/core/services/interface.py
+++ app/core/services/interface.py
@@ -15,10 +15,10 @@ class InterfaceService:
         """
 
         _, has_submitted_num = dao.Form.query_forms(query_dict={
-            "status": "已提交"
+            "status": ["已提交"]
         })
         _, wait_submitted_form_num = dao.Form.query_forms(query_dict={
-            "status": "已提交"
+            "status": ["已提交"]
         })
         units = ['林学院', '水土保持学院', '生物科学与技术学院', '园林学院', '经济管理学院', '工学院',
                  '理学院', '信息学院', '人文社会科学学院', '外语学院', '材料科学与技术学院',
@@ -26,8 +26,8 @@ class InterfaceService:
         for unit in units:
             pass
             _, submit_unit_num = dao.Form.query_forms(query_dict={
-                "status": "已提交",
-                "meta.lesson.lesson_teacher_unit": unit
+                "status": ["已提交"],
+                "meta.lesson.lesson_teacher_unit": [unit]
             })
             redis_cli.set('sys:form_num:{unit}'.format(unit=unit), json.dumps(submit_unit_num))
         redis_cli.set("sys:submitted_form", json.dumps(has_submitted_num))
