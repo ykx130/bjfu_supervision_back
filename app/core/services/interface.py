b31960cf0ae75c10eecda6ac51ed84532f7dff17
suchang <1103232282@qq.com>
2019-05-09 18:00:21 +0800

    首页数据更新

--- app/core/services/interface.py
+++ app/core/services/interface.py
@@ -15,18 +15,17 @@ class InterfaceService:
         """
 
         _, has_submitted_num = dao.Form.query_forms(query_dict={
-            "status": ["已提交"]
+            "status": ["已完成"]
         })
         _, wait_submitted_form_num = dao.Form.query_forms(query_dict={
-            "status": ["已提交"]
+            "status": ["待提交"]
         })
         units = ['林学院', '水土保持学院', '生物科学与技术学院', '园林学院', '经济管理学院', '工学院',
                  '理学院', '信息学院', '人文社会科学学院', '外语学院', '材料科学与技术学院',
                  '自然保护区学院', '环境科学与工程学院', '艺术设计学院', '体育教学部', '马克思主义学院']
         for unit in units:
-            pass
             _, submit_unit_num = dao.Form.query_forms(query_dict={
-                "status": ["已提交"],
+                "status": ["已完成"],
                 "meta.lesson.lesson_teacher_unit": [unit]
             })
             redis_cli.set('sys:form_num:{unit}'.format(unit=unit), json.dumps(submit_unit_num))
