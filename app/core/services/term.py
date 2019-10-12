6c299121fc8cc245aed3eed3f70be27005efede7
Your Name <you@example.com>
2019-10-12 16:03:05 +0800

    cached

--- app/core/services/term.py
+++ app/core/services/term.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 import app.core.dao as dao
+from app import cache
 from app.utils.Error import CustomError
 
 
 class TermService(object):
     @classmethod
+    @cache.cached(timeout=1000000000)
     def get_now_term(cls):
         date = datetime.now()
         (terms, num) = dao.Term.query_terms(query_dict={'begin_time_lte': [str(date)], 'end_time_gte': [str(date)]})
