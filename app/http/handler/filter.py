632f695775beb8f415f273b0961d9f3c31db4ac0
1103232282@qq.com <you@example.com>
2020-09-07 21:17:16 +0800

    modified:   app/http/handler/filter.py 	modified:   app/scripts/__init__.py

--- app/http/handler/filter.py
+++ app/http/handler/filter.py
@@ -78,7 +78,7 @@ class Filter(object):
                     username = user.get('username')
                     user_id = user.get('id')
                     role_names = user.get('role_names', list())
-
+                    term = query_dict.get('term')
                     supervisor_role_names = ['督导', '小组长', '大组长']
 
                     current_role = UserRoleMap.get(request.headers.get('CurrentRole', None), '教师')
@@ -100,7 +100,7 @@ class Filter(object):
                             query_dict = query_dict
                         elif current_role == '小组长':
                             supervisors, _ = SupervisorController.query_supervisors(
-                                query_dict={'group_name': [group], 'term': term})
+                                query_dict={'group_name': [group]})
                             usernames = [supervisor.get('username') for supervisor in supervisors]
                             query_dict.update(
                                 {'group_name': [group], 'username': usernames})
@@ -134,7 +134,9 @@ class Filter(object):
                     username = user.get('username')
                     role_names = user.get('role_names', list())
                     supervisor_role_names = ['督导', '小组长', '大组长']
-
+                    term = query_dict.get('meta.term')
+                    if term is None:
+                        term = TermService.get_now_term()['name']
                     current_role = UserRoleMap.get(request.headers.get('CurrentRole', None), '教师')
                     if current_role is None or current_role not in role_names:
                         return jsonify({
@@ -156,10 +158,10 @@ class Filter(object):
                             supervisors, _ = SupervisorController.query_supervisors(query_dict={'group_name': [group]})
                             usernames = [supervisor.get('username') for supervisor in supervisors]
                             query_dict.update(
-                                {'meta.guider_group': group, 'meta.guider': usernames, 'meta.term': term})
+                                {'meta.guider_group': group, 'meta.guider': usernames})
                         else:
                             query_dict.update(
-                                {'meta.guider_group': group, 'meta.guider': username, 'meta.term': term})
+                                {'meta.guider_group': group, 'meta.guider': username})
                 print(query_dict)
                 result = func(*args, **query_dict)
                 return result
--- app/scripts/__init__.py
+++ app/scripts/__init__.py
@@ -1,3 +1,13 @@
+'''
+@Author: your name
+@Date: 2019-11-22 11:39:34
+@LastEditTime: 2019-11-22 11:39:34
+@LastEditors: your name
+@Description: In User Settings Edit
+@FilePath: /bjfu_supervision_back/app/scripts/__init__.py
+'''
 import app.core.dao as dao
 import pymysql
+from app import app
+
 
